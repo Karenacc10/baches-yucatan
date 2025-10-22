2fe8b34eccc36efb665b7c0fc65a12db26495c58
Karen <karencordova2004@gmail.com>
2025-10-21 20:14:20 -0600

    fix: dynamic prisma import + add start:prod script

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,7 +1,8 @@
-import { PrismaClient } from '../generated/prisma';
-
-const prisma = new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'],
-});
-
+let PrismaClient;
+if (process.env.NODE_ENV === 'production') {
+  PrismaClient = require('../../generated/prisma').PrismaClient;
+} else {
+  PrismaClient = require('../generated/prisma').PrismaClient;
+}
+const prisma = new PrismaClient({ log: ['query', 'info', 'warn', 'error'] });
 export default prisma;
